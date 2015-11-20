549d43ab0076bb6a6af95af2ef1c35344ab4fa23
Jeff Garzik <jeff@bloq.com>
2015-11-20 04:26:22 +0000

    Change API endpoints to indicate API name and version.  Export API metadata.

--- hashfs/urls.py
+++ hashfs/urls.py
@@ -5,6 +5,10 @@ from . import views
 urlpatterns = patterns('',
 
 url(r'^$', 'hashfs.views.home', name='home'),
+
+url(r'^hashfs/1/data$', 'hashfs.views.hashfs_put', name='hashfs_put'),
+url(r'^hashfs/1/d/([a-fA-F0-9]+)$', 'hashfs.views.hashfs_get', name='hashfs_get'),
+
 url(r'^payments/', include('two1.lib.bitserv.django.urls')),
 
 )
--- hashfs/views.py
+++ hashfs/views.py
@@ -1,10 +1,49 @@
+import json
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from two1.lib.bitserv.django import payment
 
 @api_view(['GET'])
-@payment.required(1)
 def home(request):
-    body='Hello, World!'
+    # export API endpoint metadata
+    home_obj = [
+        {
+	    "name": "hashfs/1",
+	    "pricing-type": "per-rpc",
+	    "pricing" : [
+	        {
+		    "rpc": "get",
+		    "per-req": 1,         # 1 satoshi per request
+		    "per-kb": 10,         # 10 satoshis per 1000 bytes
+		},
+	        {
+		    "rpc": "put",
+		    "per-req": 1,         # 1 satoshi per request
+		    "per-kb": 10,         # 10 satoshis per 1000 bytes
+		    "per-hour": 2,        # 2 satoshis per hour to keep alive
+		},
+
+		# default pricing, if no specific match
+	        {
+		    "rpc": True,          # True = indicates default
+		    "per-req": 1,         # 1 satoshi per request
+		    "per-kb": 10,         # 10 satoshis per 1000 bytes
+		},
+	    ]
+	}
+    ]
+    body = json.dumps(home_obj)
+    return HttpResponse(body, content_type='application/json')
+
+@api_view(['GET'])
+@payment.required(1)
+def hashfs_get(request, hexstr):
+    body="Data requested for hash " + hexstr
+    return HttpResponse(body, content_type='text/plain')
+
+@api_view(['GET'])
+@payment.required(1)
+def hashfs_put(request):
+    body="Put data..."
     return HttpResponse(body, content_type='text/plain')
 
